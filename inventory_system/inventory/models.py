087068b5f29c5e859b601b3b21870ed2772686da
simratdhill0n <simratpalsinghdhillon@gmail.com>
2025-08-21 20:37:32 -0400

    [FEAT] Added a model for Product, this would save the data of the products

--- inventory_system/inventory/models.py
+++ inventory_system/inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
